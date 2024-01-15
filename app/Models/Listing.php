01df775cb2adf364993ce3b1ea2260eead33db2b
SwissDaVinci_HD <swissdavinci@gmail.com>
2024-01-15 21:30:22 +0100

    Edit Listing Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,28 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return [
-                [
-                    'id'=> '1',
-                    'title'=> 'Listing One',
-                    'description'=> 'Ut gubergren diam elitr diam et. Et consetetur invidunt dolores sed voluptua sit. Et erat dolore kasd ipsum diam diam.',
-                ],
-                [
-                    'id'=> '2',
-                    'title'=> 'Listing Two',
-                    'description'=> 'Aus neu entwöhntes jenem zerstoben widerklang auf selbst. Schwankende der der gestalten sich früh denen nun. Ersten sich es herauf.',
-                ]
-            ];
-        }
-        public static function find($id) {
-            $listings = self::all();
+namespace App\Models;
 
-            foreach($listings as $listing) {
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }   
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
