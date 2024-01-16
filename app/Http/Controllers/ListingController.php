11d82dc01d232f9c5bec07c1fddcd4a6605d43fe
SwissDaVinci_HD <swissdavinci@gmail.com>
2024-01-16 21:44:22 +0100

    Modify Routes with Controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    
+    //Show all Listings
+    public function index() {
+        return view('listings', [
+            'listings' => Listing::all()
+        ]);
+    }
+
+    //Show single Listing
+    public function show(Listing $listing) {
+        return view('listing', [
+            'listing'=> $listing
+        ]);
+    }
 }
