8c26c1688977e37f8575bc8f57433506ba823dc5
SDVHD <swissdavinci@gmail.com>
2024-01-17 13:40:21 +0100

    Fix creating listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,8 @@ class Listing extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'company', 'location' ,'website', 'description', 'email', 'tags'];
+
     public function scopeFilter($query, array $filters) {
         if ($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%'. request('tag') . '%');
