ec69bd526f56c55bb856a0d076e13b449b5ef297
SDVHD <swissdavinci@gmail.com>
2024-01-17 10:59:25 +0100

    Update Controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,14 +9,14 @@ class ListingController extends Controller
 {
     //Show all Listings
     public function index() {
-        return view('listings', [
+        return view('listings.index', [
             'listings' => Listing::all()
         ]);
     }
 
     //Show single Listing
     public function show(Listing $listing) {
-        return view('listing', [
+        return view('listings.show', [
             'listing'=> $listing
         ]);
     }
